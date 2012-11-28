38650286ca80cc9bb95e2f45cde549e304888e68
Lucy Cheng <lcheng@college.harvard.edu>
2012-11-28 02:14:21 -0500

    testing

---
+++ templates/clubs_page.php
@@ -0,0 +1,11 @@
+<form action="allClubs.php" method="post">
+    <select name="club">
+        <?php
+            foreach ($clubs as $club)
+            {
+                 print("<option>".$club["name"]."</option>");
+            }
+        ?>
+    </select>
+    <button type="submit" class="btn">Go!</button>
+
