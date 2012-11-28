2d6583b5a164ab10d7374769b3c73547182c22a2
Ramya Rangan <ramyarangan@college.harvard.edu>
2012-11-28 01:36:23 -0500

    Fixed input date/time fields.

--- templates/header.php
+++ templates/header.php
@@ -6,16 +6,20 @@
 
         <link href="css/bootstrap.css" rel="stylesheet"/>
         <link href="css/bootstrap-responsive.css" rel="stylesheet"/>
-        <link href="css/jquery-ui.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="css/jquery-ui.css" />
         <link href="css/jquery-ui-timepicker-addon.css" rel="stylesheet"/>
         <link href="css/styles.css" rel="stylesheet"/>
-
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />
+        
         <?php if (isset($title)): ?>
             <title><?= htmlspecialchars($title) ?></title>
         <?php else: ?>
             <title></title>
         <?php endif ?>
-     
+
+   
+        <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+        <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
         <script type="text/javascript" src="js/jquery-1.8.2.js"></script>
         <script type="text/javascript" src="js/jquery-ui.min.js"></script>
         <script type="text/javascript" src="js/jquery-ui-timepicker-addon.js"></script>
@@ -24,8 +28,8 @@
         <script src="js/scripts.js"></script>
         <script>
             $(document).ready(function(){
-                $(".datetime").click(function(){
-                    $(this).datetimepicker({});
+                $(".datetime").hover(function(){
+                    $(this).datetimepicker({controlType: 'select', timeFormat: 'hh:mm tt', stepMinute: 5});
                 });
             });
         </script>
