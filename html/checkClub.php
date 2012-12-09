b2ad33d2f1d373e319b912720605a8d975aacce6
Lucy Cheng <lcheng@college.harvard.edu>
2012-12-09 10:25:25 -0500

    temp

--- html/checkClub.php
+++ html/checkClub.php
@@ -9,8 +9,8 @@
         $resultName = query("SELECT * FROM clubs WHERE name = ?", $_POST["club"]);
         $resultAbbr = query("SELECT * FROM clubs WHERE abbreviation = ?", $_POST["abbreviation"]);
         $resultEmail = query("SELECT * FROM clubs WHERE email = ?", $_POST["email"]);
-        if(!empty($_POST["club"]) && $resultEmail["name"]==$_POST["club"])
-            echo json_encode(array("email" => 0));
+//        if(!empty($_POST["club"]) && $resultEmail["name"]==$_POST["club"])
+//            echo json_encode(array("email" => 0));
         
         echo json_encode(array("name" => count($resultName), "abbr" => count($resultAbbr),
                  "email" => count($resultEmail)));
