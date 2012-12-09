08b49981cca064fba7bab0496679ba43c2c75710
Ramya Rangan <ramyarangan@college.harvard.edu>
2012-12-09 03:10:36 -0500

    Oops I commited the wrong thing.

---
+++ html/contactUs.php
@@ -0,0 +1,52 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+
+    // if form was submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        require("PHPMailer/class.phpmailer.php");
+        $mail = new PHPMailer();
+
+          // use SMTP
+          $mail->IsSMTP();
+          $mail->Host = "smtp.fas.harvard.edu";
+
+          // set From:
+          $mail->SetFrom($_POST["email"], $_POST["name"]);
+
+          // set To:
+          $mail->AddAddress("cs50organizations@gmail.com");
+
+          // set Subject:
+          $mail->Subject = $_POST["subject"];
+
+          // set body
+          $mail->Body = $_POST["body"];
+
+          // send mail
+          if ($mail->Send() == false)
+          {
+              die($mail->ErrInfo);
+          }
+          
+        redirect("index.php?type=email&success=1"); 
+        
+    }
+    else
+    {
+        if(isset($_SESSION["id"]))
+        {
+            $user = query("SELECT email, realname FROM users WHERE id=?", $_SESSION["id"]);
+            $name = $user[0]["realname"];
+            $email = $user[0]["email"];
+        }
+        else
+        {
+            $name = "";
+            $email = "";
+        }
+        render_div("email_form.php", array("name" =>$name, "email" => $email));
+    }
+?>
